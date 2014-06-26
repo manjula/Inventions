0a17c1af7be65f576c9ce89ecadb1371bdf150bf
manjula <rajmanjula2006@gmail.com>
2014-06-26 17:49:14 +0530

    welcome page added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var myman = null;
+var animate ;
+function init(){
+   myman = document.getElementById('man');
+   myman.style.position= 'relative'; 
+   myman.style.left = '0px'; 
+}
+function moveRight(){
+   myman.style.left = parseInt(myman.style.left) + 1 + 'px';
+   animate = setTimeout(moveRight); 
+}
+function stop(){
+   clearTimeout(animate);
+   myman.style.position= 'relative';
+}
+function moveLeft(){
+   myman.style.position= 'relative'; 
+   myman.style.left = '0px'; 
+}
+window.onload =init;
---
+++ db/migrate/20140626103448_create_welcomes.rb
@@ -0,0 +1,8 @@
+class CreateWelcomes < ActiveRecord::Migration
+  def change
+    create_table :welcomes do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
